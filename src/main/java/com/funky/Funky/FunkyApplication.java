209e5781913e22a6f4b82ecd1a4106af06b39643
its-navneet <navneetg174@gmail.com>
2024-09-22 21:01:36 +0530

    Spring boot setup

---
+++ src/main/java/com/funky/Funky/FunkyApplication.java
@@ -0,0 +1,13 @@
+package com.funky.Funky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funky/Funky/FunkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funky.Funky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
